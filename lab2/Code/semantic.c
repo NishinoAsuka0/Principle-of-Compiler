3ba34e0b911d68b807747f56aeecaced9d70689e
NishinoAsuka0 <920993239@qq.com>
2023-10-27 01:51:48 -0700

    four

--- lab2/Code/semantic.c
+++ lab2/Code/semantic.c
@@ -194,7 +194,190 @@ Type analyseType(struct Node*node){
         }
 	return newType;
 }
-void ExtDecList(struct Node*node, Type ntype);
-void CompSt(struct Node *node, Type ntype);
-void Stmt(struct Node *node, Type ntype);
-Type StructSpecifier(struct Node* node);
+void ExtDecList(struct Node*node, Type ntype){
+	if(DEBUG_FLAG){
+		printf("Go in ExtDecList analyse\n");
+	}
+	//判断节点是否为空
+        if(node == NULL)
+                return;
+	if(Use_This_Rule(node, 1, "VarDec")){
+		if(DEBUG_FLAG)	printf("ExtDecList := VarDec \n");
+		VarDec(node->firstChild, ntype);
+	}
+	else if(Use_This_Rule(node, 3, "VarDec", "COMMA", "ExtDecList")){
+		if(DEBUG_FLAG)  printf("ExtDecList := VarDec COMMA ExtDecList \n");
+		VarDec(node->firstChild, ntype);
+		ExtDecList(node->firstChild->bro->bro, ntype);
+	}
+	else{
+		printf("Error!No this analyse!\n");
+	}
+}
+void CompSt(struct Node *node, Type ntype){
+	if(DEBUG_FLAG){
+                printf("Go in CompSt analyse\n");
+        }
+        //判断节点是否为空
+        if(node == NULL)
+                return;
+	if(Use_This_Rule(node, 4, "LC", "DefList", "StmtList", "RC")){
+                if(DEBUG_FLAG)  printf("CompSt := LC DefList StmtList RC\n");
+                DefList(node->firstChild->bro, 0);
+		StmtList(node->firstChild->bro->bro, ntype);
+        }
+	else{
+		printf("Error!No This analyse!\n");
+	}
+}
+void DefList(struct Node*node, int num){
+	if(DEBUG_FLAG){
+                printf("Go in DefList analyse\n");
+        }
+        //判断节点是否为空
+        if(node == NULL)
+                return;
+	Def(node->firstChild);
+	DefList(node->firstChild->bro);
+}
+void Def(struct Node*node){
+	if(DEBUG_FLAG){
+                printf("Go in Def analyse\n");
+        }
+        //判断节点是否为空
+        if(node == NULL)
+                return;
+	if(Use_This_Rule(node, 3, "Specifier", "DecList", "SEMI")){
+		if(DEBUG_FLAG)	printf("Def := Specifier DecList SEMI\n");
+		Type type = Specifier(node->firstChild);
+		DecList(node->firstChild->bro, type);
+	}
+	else{
+                printf("Error!No This analyse!\n");
+        }
+}
+void DecList(struct Node*node, Type ntype){
+	if(DEBUG_FLAG){
+                printf("Go in DecList analyse\n");
+        }
+        //判断节点是否为空
+        if(node == NULL)
+                return;
+	if(Use_This_Rule(node, 1, "Dec")){
+		if(DEBUG_FLAG)	printf("DecList := Dec\n");
+		Dec(node->firstChild, ntype);
+	}
+	else if(Use_This_Rule(node, 3, "Dec", "COMMA", "DecList")){
+		if(DEBUG_FLAG)	printf("DecList := Dec COMMA DecList\n");
+		Dec(node->firstChild, ntype);
+		DecList(node->firstChild->bro->bro);
+	}
+	else{
+                printf("Error!No This analyse!\n");
+        }
+}
+void Dec(struct Node*node, Type ntype){
+	if(DEBUG_FLAG){
+                printf("Go in Dec analyse\n");
+        }
+        //判断节点是否为空
+        if(node == NULL)
+                return;
+        if(Use_This_Rule(node, 1, "VarDec")){
+                if(DEBUG_FLAG)  printf("Dec := VarDec\n");
+                VarDec(node->firstChild, ntype);
+        }
+        else if(Use_This_Rule(node, 3, "VarDec", "ASSIGNOP", "Exp")){
+                if(DEBUG_FLAG)  printf("Dec := VarDec ASSIGNOP Exp\n");
+                FieldList newField = VarDec(node->firstChild, ntype);
+                Type newType = Exp(node->firstChild->bro->bro);
+        }
+        else{
+                printf("Error!No This analyse!\n");
+        }
+}
+void StmtList(struct Node*node, Type ntype){
+	if(DEBUG_FLAG){
+                printf("Go in StmtList analyse\n");
+        }
+        //判断节点是否为空
+        if(node == NULL)
+                return;
+	Stmt(node->firstChild, ntype);
+	StmtList(node->firstChild->bro, ntype);
+}
+void Stmt(struct Node *node, Type ntype){
+	if(DEBUG_FLAG){
+                printf("Go in Stmt analyse\n");
+        }
+        //判断节点是否为空
+        if(node == NULL)
+                return;
+	struct Node* child = node->firstChild;
+	while(child!=NULL){
+		if(equal_string("Exp", child->nodeName)){
+			if(DEBUG_FLAG)  printf("Go in Stmt's Exp\n");
+			Exp(node);
+		}
+		else if(equal_string("CompSt", child->nodeName)){
+			if(DEBUG_FLAG)  printf("Stmt := CompSt\n");
+			CompSt(child, ntype);
+		}
+		else if(equal_string("RETURN", child->nodeName)){
+			if(DEBUG_FLAG)  printf("Stmt := RETURN Exp SEMI\n");
+			Type expType = Exp(child->firstChild);
+			expType->possition = RIGHT;
+		}
+		else if(equal_string("WHILE", child->nodeName)){
+			if(DEBUG_FLAG)  printf("Stmt := WHILE LP Exp RP Stmt\n");
+			Exp(child->firstChild->bro);
+			Stmt(child->firstChild->bro->bro->bro, ntype);
+		}	
+		child = child->bro;
+	}
+}
+Type StructSpecifier(struct Node* node){
+	if(DEBUG_FLAG){
+                printf("Go in StructSpecifier analyse\n");
+        }
+        //判断节点是否为空
+        if(node == NULL)
+                return;
+	//是结构体定义且名字存在
+	if(Use_This_Rule(node, 5, "STRUCT", "OptTag", "LC", "DefList", "RC")){
+		if(DEBUG_FLAG)	printf("StructSpecifier := STRUCT OptTag LC DefList RC\n");
+		Type structType = (Type)malloc(sizeof(struct Type_));
+		structType->name = node->firstChild->bro->Valstr;
+		structType->kind = STRUCTURE;
+		structType->inform.structure = ;
+		structType->possition = BOTH;
+		DefList(node->firstChild->bro->bro->bro);
+}
+
+Function FunDec(struct Node*node, Type ntype){
+	if(DEBUG_FLAG){
+                printf("Go in FunDec analyse\n");
+        }
+        //判断节点是否为空
+        if(node == NULL)
+                return;
+	Function newfunc = (Function)malloc(sizeof(struct Function_));
+	struct Node* child = node->firstChild;
+	newfunc->name = child->Valstr;
+	newfunc->line = child->lineNum;
+	newfunc->type = ntype;
+	if(Use_This_Rule(node, 4, "ID", "LP", "VarList", "RP")){
+		if(DEBUG_FLAG)  printf("FunDec := ID LP VarList RP \n");
+		FieldList nextField = VarList(node->firstChild->bro->bro, ntype);
+		newfunc->next = nextField;
+	}
+	else if(Use_This_Rule(node, 3, "ID", "LP", "RP")){
+		if(DEBUG_FLAG)  printf("FunDec := ID LP RP \n");
+		newfunc->next = NULL;
+	}
+	else{
+		printf("Error!No this analyse!\n");
+	}
+	return newfunc;
+}
+FieldList VarList(struct Node* node, Type ntype);
--- lab2/Code/semantic.h
+++ lab2/Code/semantic.h
@@ -13,11 +13,20 @@ void semantic_check_start(struct Node* Root);
 //全局语法单元遍历
 void ExtDefList(struct Node *node);
 void ExtDef(struct Node* node);
+// 判断是否使用了指定的产生式
+bool Use_This_Rule(struct Node* node, int index, ...);
 //局部语句块遍历
 void CompSt(struct Node *node, Type ntype);
 void Stmt(struct Node *node, Type ntype);
-// 判断是否使用了指定的产生式
-bool Use_This_Rule(struct Node* node, int index, ...);
-TYPE Specifier(struct Node*node);
+Type Specifier(struct Node*node);
 void ExtDecList(struct Node*node,Type ntype);
+Function FunDec(struct Node*node, Type ntype);
+Type StructSpecifier(struct Node* node);
+Type analyseType(struct Node*node);
+void StmtList(struct Node*node, Type ntype);
+FieldList VarList(struct Node* node, Type ntype);
+void DefList(struct Node*node, int num);
+void Def(struct Node*node);
+void DecList(struct Node*node, Type ntype);
+void Dec(struct Node*node, Type ntype);
 
